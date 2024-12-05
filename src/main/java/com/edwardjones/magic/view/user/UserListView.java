c79ea3cd2e585a5accf489896096a90619e519e8
David Kurz <dkurz@codefusionreactor.com>
2024-12-05 15:56:08 -0600

    Initial commit.

---
+++ src/main/java/com/edwardjones/magic/view/user/UserListView.java
@@ -0,0 +1,14 @@
+package com.edwardjones.magic.view.user;
+
+import com.edwardjones.magic.entity.User;
+import com.edwardjones.magic.view.main.MainView;
+import com.vaadin.flow.router.Route;
+import io.jmix.flowui.view.*;
+
+@Route(value = "users", layout = MainView.class)
+@ViewController(id = "magic_User.list")
+@ViewDescriptor(path = "user-list-view.xml")
+@LookupComponent("usersDataGrid")
+@DialogMode(width = "64em")
+public class UserListView extends StandardListView<User> {
+}
